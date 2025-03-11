fb6ead340efe6cacdce2e35a62a213c940bd264f
AmitPathak <amit7081483564@gmail.com>
2025-03-11 17:30:50 +0530

    Implement form validation and error handling for user input; add Uppercase validation rule

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -7,19 +7,40 @@ use Illuminate\Http\Request;
 class FormController extends Controller
 {
     function userForm(Request $req){
-        echo "User name is: $req->name";
-        echo "<br>";
-        echo "User email is: $req->email";
-        echo "<br>";
-        echo "User city is: $req->city";
-        echo "<br>";
-        echo "User gender is: $req->gender";
-        echo "<br>";
-        echo "User state is: $req->state";
-        echo "<br>";
-        echo "User age is: $req->age";
-        echo "<br>";
-        print_r($req->skill);
-        // return $req;
+
+        $req->validate([
+            'name' => 'required|min:3|max:100',
+            'email' => 'required|email',
+            'city' => 'required|uppercase',
+            'gender' => 'required',
+            'skill' => 'required',
+        ],
+        // Custom error message it self
+        [
+            'name.required' => 'Please enter your name',
+            'name.min' => 'Name must be at least 3 characters',
+            'name.max' => 'Name must be at most 10 characters',
+            'email.required' => 'Please enter your email',
+            'email.email' => 'Please enter a valid email',
+            'city.required' => 'Please enter your city',
+        ]);
+
+        // echo "User name is: $req->name";
+        // echo "<br>";
+        // echo "User email is: $req->email";
+        // echo "<br>";
+        // echo "User city is: $req->city";
+        // echo "<br>";
+        // echo "User gender is: $req->gender";
+        // echo "<br>";
+        // echo "User skill is: $req->skill";
+
+
+        // echo "User city is: $req->city";
+        // echo "<br>";
+        // echo "User age is: $req->age";
+        // echo "<br>";
+        // print_r($req->skill);
+        return $req;
     }
 }
--- resources/views/user-form.blade.php
+++ resources/views/user-form.blade.php
@@ -1,47 +1,65 @@
 <div class="container-fiuld">
     <p class="title">Add New User</p>
 
+
+    <!-- @if($errors->any())
+    @foreach($errors->all() as $error)
+    <p style="color:red">{{$error}}</p>
+    @endforeach
+    @endif -->
     <form action="userform" method="post">
         @csrf
         <div class="input-wrapper">
-            <input class="form" type="text" name="name" placeholder="Enter Name" />
+            <input type="text" name="name" placeholder="Enter Name" value="{{old('name')}}"
+                class="{{ $errors->first('name')?'input-error':'' }}" />
+            <span style="color:red">@error('name'){{$message}}@enderror</span>
         </div>
         <div class="input-wrapper">
-            <input class="form" type="text" name="email" placeholder="Enter email" />
+            <input type="text" name="email" placeholder="Enter email" value="{{old('email')}}"
+                class="{{$errors->first('email')?'input-error':''}}" />
+            <span style="color:red">@error('email'){{$message}}@enderror</span>
         </div>
         <div class="input-wrapper">
-            <input class="form" type="text" name="state" placeholder="Enter state" />
+            <input type="text" name="city" placeholder="Enter city" value="{{old('city')}}"
+                class="{{$errors->first('city')?'input-error':''}}" />
+            <span style="color:red">@error('city'){{$message}}@enderror</span>
         </div>
         <div>
             <h3>Skills</h3>
-            <input type="checkbox" name="skill[]" value="php" id="php" />
+            <input type="checkbox" name="skill" value="php" id="php"
+                class="{{$errors->first('skill')?'input-error':''}}" />
             <label for="php">PHP</label>
-            <input type="checkbox" name="skill[]" value="java" id="java" />
+            <input type="checkbox" name="skill" value="java" id="java"
+                class="{{$errors->first('skill')?'input-error':''}}" />
             <label for="java">Java</label>
-            <input type="checkbox" name="skill[]" value="react" id="react" />
+            <input type="checkbox" name="skill" value="react" id="react"
+                class="{{$errors->first('skill')?'input-error':''}}" />
             <label for="react">React</label>
         </div>
+        <span style="color:red">@error('skill'){{$message}}@enderror</span>
         <div>
             <h3>Gender</h3>
-            <input type="radio" name="gender" value="male" id="male"/>
+            <input type="radio" name="gender" value="male" id="male"
+                class="{{$errors->first('gender')?'input-error':''}}" />
             <label for="male">Male</label>
-            
-            <input type="radio" name="gender" value="female" id="female"/>
+
+            <input type="radio" name="gender" value="female" id="female"
+                class="{{$errors->first('gender')?'input-error':''}}" />
             <label for="female">Female</label>
-            
         </div>
-        <div>
+        <span style="color:red">@error('gender'){{$message}}@enderror</span>
+        <!-- <div>
             <h3>City</h3>
-            <select name="city">
-                <option value="azamgarh">Azamgarh</option>
-                <option value="jaunpur">Jaunpur</option>
-                <option value="varansi">Varansi</option>
+            <select name="state">
+                <option value="azamgarh">Utter Pradesh</option>
+                <option value="jaunpur">Mumbai</option>
+                <option value="varansi">Delhi</option>
             </select>
-        </div>
-        <div>
+        </div> -->
+        <!-- <div>
             <h3>Age</h3>
             <input type="range" name="age" min="18" max="100" />
-        </div>
+        </div> -->
         <div class="input-wrapper">
             <button>Add new user</button>
         </div>
@@ -49,7 +67,7 @@
 </div>
 
 <style>
-    .container-fiuld{
+    .container-fiuld {
         width: 225px;
         margin: 0 auto;
         padding-left: 20px;
@@ -58,16 +76,29 @@
         border-radius: 5px;
         margin-top: px;
     }
-    .title{
+
+    .title {
         font-size: 30px;
         text-align: center;
+        font-weight: bold;
     }
-    .form {
+
+    input {
         border: orange 1px solid;
         height: 35px;
         width: 200px;
         border-radius: 2px;
         color: orange;
+    }
+
+    input[type="checkbox"] {
+        height: 20px;
+        width: 40px;
+    }
+
+    input[type="radio"] {
+        height: 20px;
+        width: 40px;
     }
 
     .input-wrapper {
@@ -82,5 +113,12 @@
         width: 200px;
         color: orange;
         cursor: pointer;
+    }
+
+    .input-error {
+        border: 1px solid red !important;
+        /* 🔥 Force apply */
+        color: red !important;
+        border-radius: 2px;
     }
 </style>
